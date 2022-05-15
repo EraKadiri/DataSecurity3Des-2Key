5ee76b534331b3a1e04f966f7b447efd3b2760d3
EraKadiri <78500922+EraKadiri@users.noreply.github.com>
2022-05-15 21:21:06 +0200

    Decrypt 2-key

--- TripleDes.cs
+++ TripleDes.cs
@@ -53,3 +53,43 @@ namespace _3DES
             return Convert.ToBase64String(MyresultArray, 0,
                MyresultArray.Length); ;
         }
+        
+        public static string Decrypt(string TextToDecrypt)
+        {
+            byte[] IV = Encoding.Default.GetBytes(iv);
+
+            byte[] MyDecryptArray = Convert.FromBase64String
+               (TextToDecrypt);
+
+            MD5CryptoServiceProvider MyMD5CryptoService = new
+               MD5CryptoServiceProvider();
+
+            byte[] MysecurityKeyArray = MyMD5CryptoService.ComputeHash
+               (UTF8Encoding.UTF8.GetBytes(securityKey));
+
+            MyMD5CryptoService.Clear();
+
+            var MyTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+
+            MyTripleDESCryptoService.Key = MysecurityKeyArray;
+
+            MyTripleDESCryptoService.Mode = CipherMode.CBC;
+
+            MyTripleDESCryptoService.IV = IV;
+
+            MyTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+                
+
+            var MyCrytpoTransform = MyTripleDESCryptoService
+               .CreateDecryptor();
+
+            byte[] MyresultArray = MyCrytpoTransform
+               .TransformFinalBlock(MyDecryptArray, 0,
+               MyDecryptArray.Length);
+
+            MyTripleDESCryptoService.Clear();
+
+            return UTF8Encoding.UTF8.GetString(MyresultArray);
+        }
+    }
+}
